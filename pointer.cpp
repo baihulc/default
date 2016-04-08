29be9b40b205d38d51b8b36da74d2307e92782d1
baihulc <baihulc@gmail.com>
2016-04-08 10:27:28 +0800

    拆分

--- pointer.cpp
+++ pointer.cpp
@@ -1,24 +1,8 @@
-#include "stdfx.h"
-#include "iostream"
-
-using namespace std
-
-class Point {
-public:
-	void  SetPointer(int x, int y){
-		m=x;n=y;
-	}
-	void GetPointer(){
-		cout<<m;
-		cout<<n;
-	}
-
-private:
-	int m,n;
-};
+#include "ClassPoint.cpp"
 
 int main(){
+	
 	Point p;
 	p.SetPointer(1,2);
-	p.GetPointer
+	p.GetPointer();
 }
